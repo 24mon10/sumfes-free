d0cbc0d2e4556f99a44bf2403491ec15eed5b248
hmj2422002 <hmj2422002@stu.o-hara.ac.jp>
2025-08-18 12:23:05 +0900

    UIをちょっといじった

---
+++ Assets/Script/ButtonOn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonOn : MonoBehaviour
+{
+	//[SerializeField] Button focusButton;
+ //   // Start is called before the first frame update
+ //   void Start()
+ //   {
+ //       focusButton
+ //   }
+
+ //   // Update is called once per frame
+ //   void Update()
+ //   {
+        
+ //   }
+}
