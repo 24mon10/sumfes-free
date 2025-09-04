2b12872121f0db135b2c57ea7cbbefcd2a669996
hmj2422002 <hmj2422002@stu.o-hara.ac.jp>
2025-09-04 15:17:37 +0900

    エラー修正

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
 
 public class EnemyController : MonoBehaviour
 {
